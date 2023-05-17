24e2088611dc0db8ca1aea4b25d6b8f6d48de258
Luis Mosquera <ferchomn@gmail.com>
2023-05-16 19:19:11 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+import { Component } from "react";
+import { Map, GoogleApiWrapper } from "google-maps-react";
+
+class MapPruebaHumboldt extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        //Coordenadas geográficas de Colombia
+        initialCenter={{
+          lat: 4.570868, //Latitud Colombia
+          lng: -74.297333 //Longitud Colombia
+        }}
+      />
+    );
+  }
+}
+// LLave privada "apiKey" que brinda google en google.cloud.com
+/* Previamente se creó una cuenta con verificación de tarjeta de
+crédito, por si se genera tráfico hacer un cobro por parte de 
+google */
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyB25aNOkv4WCqvqJoajkQ2mLU2sstOoss8"
+})(MapPruebaHumboldt);
